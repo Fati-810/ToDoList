b8fda943900280dff7daeaf879e7546557ba2545
Fati-810 <49760018+Fati-810@users.noreply.github.com>
2025-05-12 14:46:21 +0500

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(express.static("public"));
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true
     , useUnifiedTopology: true
 }).then(() => {
